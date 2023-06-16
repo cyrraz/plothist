84ef7853cdad77bfa0b8ac01e07dcc50fb055c17
Tristan Fillinger <tristan.fillinger@kek.jp>
2023-06-16 15:10:51 +0900

    doc version

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = "Cyrille Praz, Tristan Fillinger"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.0"
+release = "0.2"
 
 
 # -- General configuration ---------------------------------------------------
