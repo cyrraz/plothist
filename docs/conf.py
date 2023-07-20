93a77922101a06bada3167b936800cb13c21c6bb
cyrraz <code.cyrraz@protonmail.com>
2023-07-20 15:59:00 +0900

    v0.5.4

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ copyright = "2023, Cyrille Praz, Tristan Fillinger"
 author = "Cyrille Praz, Tristan Fillinger"
 
 # The short X.Y version
-version = "0.5.3"
+version = "0.5.4"
 # The full version, including alpha/beta/rc tags
-release = "0.5.3"
+release = "0.5.4"
 
 
 # -- General configuration ---------------------------------------------------
