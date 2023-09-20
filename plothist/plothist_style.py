e4952e45a3fc30589d9c3d01bab8e1d5b33c6cc8
Tristan Fillinger <tristan.fillinger@kek.jp>
2023-09-20 15:46:22 +0900

    automatic resize of the ylabel

--- plothist/plothist_style.py
+++ plothist/plothist_style.py
@@ -35,3 +35,11 @@ def set_style(style="default"):
             plt.style.use(style_file.as_posix())
     else:
         raise ValueError(f"{style} not in the available styles: {available_styles}")
+
+
+def get_fitting_ylabel_fontsize(ax):
+    ylabel_fontsize = ax.yaxis.get_label().get_fontsize()
+    while ax.yaxis.get_label().get_window_extent().transformed(ax.transData.inverted()).y1 > ax.get_ylim()[1]:
+        ylabel_fontsize -= .1
+        ax.get_yaxis().get_label().set_size(ylabel_fontsize)
+    return ylabel_fontsize
