0f56350d52f8112c1913596058fd8ce756138910
Tristan Fillinger <tristan.fillinger@kek.jp>
2023-11-28 16:32:47 +0900

    test of gallery

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ release = "0.9"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.doctest", "sphinx.ext.autodoc", "sphinx.ext.napoleon"'sphinx_gallery.gen_gallery','sphinx.ext.intersphinx']
+extensions = ["sphinx.ext.doctest", "sphinx.ext.autodoc", "sphinx.ext.napoleon", 'sphinx_gallery.gen_gallery','sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
