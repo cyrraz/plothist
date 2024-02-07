395b1bd5ddadb1ee3cf881c9b6a0f822e65ad7ba
Tristan Fillinger <tristan.fillinger@kek.jp>
2024-02-07 12:21:35 +0900

    Use expanduser from Path

--- plothist/scripts/install_latin_modern_fonts.py
+++ plothist/scripts/install_latin_modern_fonts.py
@@ -1,23 +1,37 @@
 import subprocess
 import os
 import platform
-from pathlib import Path
+from pathlib import PosixPath
 
 
 def install_latin_modern_fonts():
     """
-    Install Latin Modern fonts for matplotlib and clean the font cache.
+    Install Latin Modern Math, Latin Modern Roman and Latin Modern Sans fonts.
+    The font cache of matplotlib is removed, so that the new fonts are available in matplotlib.
+
+    The Latin Modern Math font is available at http://mirrors.ctan.org/fonts/lm-math/opentype/latinmodern-math.otf
+    The Latin Modern Roman and Latin Modern Sans fonts are available at https://www.1001fonts.com/latin-modern-roman-font.html and https://www.1001fonts.com/latin-modern-sans-font.html
+
+    This function is only implemented for Linux and MacOS.
+
+    Raises:
+    ------
+        NotImplementedError: If the function is called on an unsupported operating system.
     """
     if platform.system() == "Linux":  # Linux
-        font_directory = Path(os.path.expanduser("~/.fonts/"))
+        font_directory = PosixPath("~/.fonts/").expanduser()
         font_directory.mkdir(parents=True, exist_ok=True)
-        matplotlib_font_cache = Path(
-            os.path.expanduser("~/.cache/matplotlib/fontlist-v330.json")
-        )
-    if platform.system() == "Darwin":  # MacOS
-        font_directory = Path(f"/Users/{os.getlogin()}/Library/Fonts")
-        matplotlib_font_cache = Path(
-            os.path.expanduser("~/.matplotlib/fontlist-v330.json")
+        matplotlib_font_cache = PosixPath(
+            "~/.cache/matplotlib/fontlist-v330.json"
+        ).expanduser()
+    elif platform.system() == "Darwin":  # MacOS
+        font_directory = PosixPath(f"/Users/{os.getlogin()}/Library/Fonts").expanduser()
+        matplotlib_font_cache = PosixPath(
+            "~/.matplotlib/fontlist-v330.json"
+        ).expanduser()
+    else:
+        raise NotImplementedError(
+            f"This script is only implemented for Linux and MacOS. If you manage to make it work on {platform.system()}, please submit a pull request."
         )
 
     # Install Latin Modern Math
@@ -25,7 +39,7 @@ def install_latin_modern_fonts():
         [
             "wget",
             "-P",
-            font_directory.as_posix(),
+            font_directory,
             "http://mirrors.ctan.org/fonts/lm-math/opentype/latinmodern-math.otf",
         ]
     )
@@ -35,7 +49,7 @@ def install_latin_modern_fonts():
             [
                 "wget",
                 "-P",
-                font_directory.as_posix(),
+                font_directory,
                 f"https://www.1001fonts.com/download/latin-modern-{lm}.zip",
             ]
         )
@@ -43,25 +57,23 @@ def install_latin_modern_fonts():
             [
                 "unzip",
                 "-o",
-                (font_directory / f"latin-modern-{lm}.zip").as_posix(),
+                (font_directory / f"latin-modern-{lm}.zip"),
                 "-d",
-                (font_directory / f"latin-modern-{lm}").as_posix(),
+                (font_directory / f"latin-modern-{lm}"),
             ]
         )
-        subprocess.run(
-            ["rm", "-f", (font_directory / f"latin-modern-{lm}.zip").as_posix()]
-        )
+        subprocess.run(["rm", "-f", (font_directory / f"latin-modern-{lm}.zip")])
 
     # Remove font cache
     try:
         subprocess.run(
-            ["rm", "-v", matplotlib_font_cache.as_posix()],
+            ["rm", "-v", matplotlib_font_cache],
             check=True,
         )
     except subprocess.CalledProcessError:
         print(
             f"""
-            Error while trying to remove {matplotlib_font_cache.as_posix()}, but maybe this is not needed.
+            Error while trying to remove {matplotlib_font_cache}, but maybe this is not needed.
             Check whether the Latin Modern fonts are now available in your matplotlib.
             If they are not, find the correct fontlist-XXX.json file in your matplotlib cache and remove it manually.
             """
