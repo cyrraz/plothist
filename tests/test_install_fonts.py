6707d3f95385e5d67b591fc1382b567171c8b861
Tristan Fillinger <tristan.fillinger@kek.jp>
2024-02-27 11:32:57 +0900

    debugging the test

--- tests/test_install_fonts.py
+++ tests/test_install_fonts.py
@@ -16,12 +16,26 @@ def test_install_latin_modern_fonts():
     for elt in findSystemFonts(fontpaths=None, fontext="ttf"):
         print("> ",elt)
 
+    print(findfont("DejaVuSerif", fallback_to_default=True))
+    print(findfont("www", fallback_to_default=True))
+
+    # print the current user
+    import os
+    print("current user ", os.getlogin())
+    # print the current system
+    import platform
+    print("current platform ", platform.system())
+    # print the current directory
+    print("current directory ", os.getcwd())
+    # print the current home directory
+    print("current home directory ", os.path.expanduser("~"))
+
     install_latin_modern_fonts(font_directory="/usr/share/fonts/truetype/")
 
     for font_type in ["Math", "Sans", "Roman"]:
         try:
             findfont(f"Latin Modern {font_type}", fallback_to_default=False)
         except ValueError:
-            fail(f"The font {font_type} was not found.")
+            fail(f"The font Latin Modern {font_type} was not found.")
 
     assert True
