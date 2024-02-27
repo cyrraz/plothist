b38f32387d6af5848e5f991ac2ebcd51fb7dcb6a
Tristan Fillinger <tristan.fillinger@kek.jp>
2024-02-27 15:11:39 +0900

    code cleaning

--- tests/test_install_fonts.py
+++ tests/test_install_fonts.py
@@ -1,6 +1,7 @@
 from pytest import fail
 from plothist.scripts import install_latin_modern_fonts
 
+
 def test_install_latin_modern_fonts():
     """
     Test install_latin_modern_fonts.
@@ -10,12 +11,15 @@ def test_install_latin_modern_fonts():
 
     # Reload the matplotlib cache
     from matplotlib.font_manager import _load_fontmanager
+
     new_fontmanager = _load_fontmanager(try_read_cache=False)
 
     installation_failed = False
     for font_type in ["Math", "Sans", "Roman"]:
         try:
-            new_fontmanager.findfont(f"Latin Modern {font_type}", fallback_to_default=False)
+            new_fontmanager.findfont(
+                f"Latin Modern {font_type}", fallback_to_default=False
+            )
         except ValueError:
             print(f"The font Latin Modern {font_type} was not found.")
             installation_failed = True
