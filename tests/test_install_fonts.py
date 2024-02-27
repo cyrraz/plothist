70208b47e529f907ea0795f8cd30855e12dbeffe
Tristan Fillinger <tristan.fillinger@kek.jp>
2024-02-27 11:06:52 +0900

    debugging the test

--- tests/test_install_fonts.py
+++ tests/test_install_fonts.py
@@ -7,6 +7,13 @@ def test_install_latin_modern_fonts():
     """
     Test install_latin_modern_fonts.
     """
+    import plothist
+
+    print("plothist folder ", plothist.__file__)
+
+    from matplotlib.font_manager import findSystemFonts
+    print(findSystemFonts(fontpaths=None, fontext="ttf"), " findSystemFonts")
+
     install_latin_modern_fonts()
 
     for font_type in ["Math", "Sans", "Roman"]:
