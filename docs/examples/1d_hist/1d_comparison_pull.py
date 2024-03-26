c2445250568ba60aaaad653f67db3482add07440
Tristan Fillinger <tristan.fillinger@kek.jp>
2024-03-26 15:54:59 +0900

    For 1d simple hists: changed from c2 and c3 label to h1 and h2

--- docs/examples/1d_hist/1d_comparison_pull.py
+++ docs/examples/1d_hist/1d_comparison_pull.py
@@ -11,26 +11,26 @@ df = get_dummy_data()
 
 name = "variable_1"
 
-x2 = df[name][df["category"] == 2]
-x3 = df[name][df["category"] == 3]
+x1 = df[name][df["category"] == 2]
+x2 = df[name][df["category"] == 3]
 
-x_range = (min(min(x2), min(x3)), max(max(x2), max(x3)))
+x_range = (min(min(x1), min(x2)), max(max(x1), max(x2)))
 
 from plothist import make_hist
 
+h1 = make_hist(x1, bins=50, range=x_range)
 h2 = make_hist(x2, bins=50, range=x_range)
-h3 = make_hist(x3, bins=50, range=x_range)
 
 ###
 from plothist import plot_two_hist_comparison
 
 fig, ax_main, ax_comparison = plot_two_hist_comparison(
+    h1,
     h2,
-    h3,
     xlabel=name,
     ylabel="Entries",
-    h1_label="$c_2$",
-    h2_label="$c_3$",
+    h1_label="$h_1$",
+    h2_label="$h_2$",
     comparison="pull",  # <---
 )
 
