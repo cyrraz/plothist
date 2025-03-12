333a7c76ca3cf1edddaaf9ba42c0f0951ae1a3dd
cyrraz <code.cyrraz@protonmail.com>
2025-03-12 13:59:20 +0900

    test: fix test of examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -49,7 +49,11 @@ scripts = [
 
 @pytest.mark.parametrize("script", scripts)
 def test_examples(script):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        os.cd(tmpdirname)
-        result = subprocess.run(["python", script], capture_output=True, text=True, env=env)
+    original_directory = os.getcwd()
+    with tempfile.TemporaryDirectory(dir=os.getcwd()) as tmpdirname:
+        os.chdir(tmpdirname)
+        script_name = os.path.basename(script)
+        os.system(f"cp {original_directory}/{script} {script_name}")
+        result = subprocess.run(["python", script_name], capture_output=True, text=True)
+        os.chdir(original_directory)
         assert result.returncode == 0, f"Script {script} failed with error: {result.stderr}"
