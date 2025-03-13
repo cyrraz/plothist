c261e3ff7182774d9e55870fbabb4a62060da03d
cyrraz <code.cyrraz@protonmail.com>
2025-03-13 12:03:57 +0900

    test: add test of each example script

---
+++ tests/test_examples.py
@@ -0,0 +1,21 @@
+import subprocess
+import pytest
+import tempfile
+import os
+import glob
+
+scripts = glob.glob("docs/examples/**/*.py", recursive=True)
+
+
+@pytest.mark.parametrize("script", scripts)
+def test_examples(script):
+    original_directory = os.getcwd()
+    with tempfile.TemporaryDirectory(dir=os.getcwd()) as tmpdirname:
+        os.chdir(tmpdirname)
+        script_name = os.path.basename(script)
+        os.system(f"cp {original_directory}/{script} {script_name}")
+        result = subprocess.run(["python", script_name], capture_output=True, text=True)
+        os.chdir(original_directory)
+        assert (
+            result.returncode == 0
+        ), f"Script {script} failed with error: {result.stderr}"
