49e5d5dedf1bfa04526f80267f18f2db7ba7191f
Tristan Fillinger <tristan.fillinger@kek.jp>
2025-07-30 17:55:44 +0900

    fix: return tuples for color palettes

--- src/plothist/plothist_style.py
+++ src/plothist/plothist_style.py
@@ -112,7 +112,7 @@ def cubehelix_palette(
     pal = cmap(x)[:, :3].tolist()
     if reverse:
         pal = pal[::-1]
-    return pal
+    return [tuple(c) for c in pal]
 
 
 def get_color_palette(
@@ -170,7 +170,8 @@ def get_color_palette(
         )
 
     plt_cmap = plt.get_cmap(cmap)
-    return plt_cmap(np.linspace(0, 1, N))
+    plt_cmap = plt_cmap(np.linspace(0, 1, N))
+    return [tuple(k) for k in plt_cmap]
 
 
 def set_fitting_ylabel_fontsize(ax: plt.Axes) -> float:
